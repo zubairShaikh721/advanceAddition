f3e7e0d127126dc05302e22918a06b362f916af7
zubairShaikh721 <shaikh.zubair721@hotmail.com>
2015-07-24 16:38:44 +0530

    new folder for new module
    something something

---
+++ index.js
@@ -0,0 +1,15 @@
+/*!
+ * Addition Trial
+ * Copyright(c) 22015-2016 TJ Holowaychuk
+ * Copyright(c) 2015 Roman Shtylman
+ * Copyright(c) 2015-2016 Zubair Lee Pranav
+ * Extentia Licensed
+ */
+
+'use strict';
+module.exports = require('./require_module.js');
+
+//var add_module=require('./require_module');
+//add_module.advanceAddition(15,50);
+// advanceAddition(34,44);
+
---
+++ require_module.js
@@ -0,0 +1,4 @@
+exports.advanceAddition=function(a,b){
+  console.log("Working yaar");
+  console.log(a+b);
+};
